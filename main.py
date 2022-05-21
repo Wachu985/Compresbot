f55538b934d71c04791aaa7bc72b15897554675d
Wachu985 <93799115+Wachu985@users.noreply.github.com>
2022-05-21 05:07:13 -0400

    main

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ bot_token  = '5336546424:AAEN7ioWpVTWjBTXAy2ZrTtLpDnqLF2IxOE'
 bot = Client("LocoBot", api_id, api_hash,bot_token=bot_token)
 
 
-yturls = list()
 Conversation(bot)
 # def progress_func(current,total,falta):
 #     print(f'{current * 100 / total:.1f}')
 
 def build_menu(buttons,n_cols,header_buttons=None,footer_buttons=None):
+    menu = []
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
     if header_buttons:
         menu.insert(0, header_buttons)
@@ -124,8 +124,8 @@ try:
         formats = yt.streams.filter(file_extension='mp4')
         formats = formats.order_by('resolution')
         title = yt.title 
+        yturls = []
         for f in formats:
-            global yturls
             yturls.append(str(str(f).split(sep=' ')[1].split(sep='"')[1]+':'+str(f).split(sep=' ')[3].split(sep='"')[1]))
         button_list = []
         for each in yturls:

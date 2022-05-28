7c17ae8c0f47b1549fdc0273d5c07da5a3f467b6
Wachu985 <93799115+Wachu985@users.noreply.github.com>
2022-05-27 22:26:41 -0400

    main

--- servidor.py
+++ servidor.py
@@ -21,4 +21,4 @@ def getFiles(name_files,username):
 app.register_blueprint(routes_files)
 
 if __name__ == '__main__':
-    app.run(debug=True,port = '80',host= '0.0.0.0')
+    app.run(debug=True,host= '0.0.0.0',port = '80')

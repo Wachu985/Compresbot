c5932af1a112a652753edd342305dd851d72c75e
Wachu985 <93799115+Wachu985@users.noreply.github.com>
2022-06-18 23:50:36 -0400

    main

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ def compresionbot(bot,msg,client,save,zips):
             msg.edit_text('🚫Tiempo de Espera Exedido🚫')
             return
         file = name.text + '.zip'
+        print(os.path.getsize(save))
         msg = bot.send_message(msg.chat.id,'📚Comprimiendo Archivos')
         comprimio,partes = split(compresion(file,save),'./',getBytes(zips))
         subidas = str(partes -1)
